4546e5c100934c38823259dd97e47922fe0fa56d
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-04 21:15:18 -0500

    todo items colored based on status

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -5,18 +5,22 @@ import ReactTooltip from "react-tooltip";
 const TodoItem = (props) => {
   const { name, status, id, deleteTodoHandler } = props;
   let statusIcon;
+  let todoItemClass = "flex space justify-between flex-row my-2 p-5 shadow-md";
 
   if (status === "not started") {
+    todoItemClass += " bg-gray-200";
     statusIcon = <PlayCircle data-tip="Not started" />;
   } else if (status === "in progress") {
+    todoItemClass += " bg-yellow-300";
     statusIcon = <Circle data-tip="In progress" />;
   } else if (status === "complete") {
+    todoItemClass += " bg-green-200";
     statusIcon = <CheckCircle data-tip="Completed" />;
   }
 
   const deleteTodo = (id) => deleteTodoHandler(id);
   return (
-    <div className="flex space justify-between flex-row my-2 p-5 bg-gray-200 shadow-md">
+    <div className={todoItemClass}>
       <div>
         <p>{name}</p>
       </div>
