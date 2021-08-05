7b9cca78e63152e4905ce5de7c3a940474918356
Jason Fritsche <fritsche.jason@gmail.com>
2021-08-04 20:59:28 -0500

    added item delete logic

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -3,8 +3,9 @@ import { Trash2, PlayCircle, CheckCircle, Circle } from "react-feather";
 import ReactTooltip from "react-tooltip";
 
 const TodoItem = (props) => {
-  const { name, status, id } = props;
+  const { name, status, id, deleteTodoHandler } = props;
   let statusIcon;
+
   if (status === "not started") {
     statusIcon = <PlayCircle data-tip="Not started" />;
   } else if (status === "in progress") {
@@ -12,6 +13,8 @@ const TodoItem = (props) => {
   } else if (status === "complete") {
     statusIcon = <CheckCircle data-tip="Completed" />;
   }
+
+  const deleteTodo = (id) => deleteTodoHandler(id);
   return (
     <div className="flex space justify-between flex-row my-2 p-5 bg-gray-200 shadow-md">
       <div>
@@ -20,7 +23,7 @@ const TodoItem = (props) => {
       <div className="flex flex-row">
         {statusIcon}
         <ReactTooltip />
-        <Trash2 data-tip="Remove" />
+        <Trash2 data-tip="Remove" onClick={() => deleteTodo(id)} />
         <ReactTooltip />
       </div>
     </div>
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  const todoItems = [
+  const [list, updateList] = useState([
     { name: "feed Gene", status: "in progress", id: 11234 },
     { name: "Have snacc", status: "complete", id: 11235 },
     { name: "wash the car", status: "not started", id: 11224 },
     { name: "clean the toilet", status: "not started", id: 11734 },
-  ];
+  ]);
+
+  const deleteTodoFromList = (id) => {
+    updateList(list.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="w-3/4 ">
       <h1 className="text-red-500">TodoList</h1>
-      {todoItems.map((todo) => (
-        <TodoItem name={todo.name} status={todo.status} id={todo.id} />
+      {list.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          name={todo.name}
+          status={todo.status}
+          id={todo.id}
+          deleteTodoHandler={deleteTodoFromList}
+        />
       ))}
     </div>
   );
